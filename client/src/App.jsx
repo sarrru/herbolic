511b56bf78935a9f9c2057703c54234d07236971
sarrru <sharusura123@gmail.com>
2025-07-25 21:42:33 +0545

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,88 +1,119 @@
-import { Outlet, useLocation } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Outlet, useLocation } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import toast, { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 import fetchUserDetails from './utils/fetchUserDetails';
 import { setUserDetails } from './store/userSlice';
-import { setAllCategory,setAllSubCategory,setLoadingCategory } from './store/productSlice';
+import {
+  setAllCategory,
+  setAllSubCategory,
+  setLoadingCategory,
+} from './store/productSlice';
 import { useDispatch } from 'react-redux';
 import Axios from './utils/Axios';
 import SummaryApi from './common/SummaryApi';
-import { handleAddItemCart } from './store/cartProduct'
 import GlobalProvider from './provider/GlobalProvider';
-import { FaCartShopping } from "react-icons/fa6";
 import CartMobileLink from './components/CartMobile';
 
 function App() {
-  const dispatch = useDispatch()
-  const location = useLocation()
-  
+  const dispatch = useDispatch();
+  const location = useLocation();
 
-  const fetchUser = async()=>{
-      const userData = await fetchUserDetails()
-      dispatch(setUserDetails(userData.data))
-  }
+  const fetchUser = async () => {
+    const userData = await fetchUserDetails();
+    dispatch(setUserDetails(userData.data));
+  };
 
-  const fetchCategory = async()=>{
+  const fetchCategory = async () => {
     try {
-        dispatch(setLoadingCategory(true))
-        const response = await Axios({
-            ...SummaryApi.getCategory
-        })
-        const { data : responseData } = response
+      dispatch(setLoadingCategory(true));
+      const response = await Axios({ ...SummaryApi.getCategory });
+      const { data: responseData } = response;
 
-        if(responseData.success){
-           dispatch(setAllCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
-        }
+      if (responseData.success) {
+        dispatch(
+          setAllCategory(
+            responseData.data.sort((a, b) => a.name.localeCompare(b.name))
+          )
+        );
+      }
     } catch (error) {
-        
-    }finally{
-      dispatch(setLoadingCategory(false))
+      console.error(error);
+    } finally {
+      dispatch(setLoadingCategory(false));
     }
-  }
+  };
 
-  const fetchSubCategory = async()=>{
+  const fetchSubCategory = async () => {
     try {
-        const response = await Axios({
-            ...SummaryApi.getSubCategory
-        })
-        const { data : responseData } = response
+      const response = await Axios({ ...SummaryApi.getSubCategory });
+      const { data: responseData } = response;
 
-        if(responseData.success){
-           dispatch(setAllSubCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
-        }
+      if (responseData.success) {
+        dispatch(
+          setAllSubCategory(
+            responseData.data.sort((a, b) => a.name.localeCompare(b.name))
+          )
+        );
+      }
     } catch (error) {
-        
-    }finally{
+      console.error(error);
     }
-  }
+  };
 
-  
-
-  useEffect(()=>{
-    fetchUser()
-    fetchCategory()
-    fetchSubCategory()
+  useEffect(() => {
+    fetchUser();
+    fetchCategory();
+    fetchSubCategory();
     // fetchCartItem()
-  },[])
+  }, []);
 
   return (
-    <GlobalProvider> 
-      <Header/>
-      <main className='min-h-[78vh]'>
-          <Outlet/>
+    <GlobalProvider>
+      <Header />
+      <main className="min-h-[78vh]">
+        <Outlet />
       </main>
-      <Footer/>
-      <Toaster/>
-      {
-        location.pathname !== '/checkout' && (
-          <CartMobileLink/>
-        )
-      }
+      <Footer />
+
+      {/* Toast Notification – Positioned just below header */}
+      <Toaster
+        position="top-center"
+        containerStyle={{
+          top: '5.5rem', // ~88px: adjust based on header height
+          zIndex: 9999,
+        }}
+        toastOptions={{
+          style: {
+            background: '#ffffff',
+            color: '#1a1a1a',
+            fontWeight: 600,
+            fontSize: '14px',
+            border: '1px solid #ddd',
+            boxShadow: '0 4px 12px rgba(0, 0, 0, 0.15)',
+            padding: '12px 16px',
+            borderRadius: '8px',
+          },
+          success: {
+            iconTheme: {
+              primary: '#22c55e',   // Tailwind green-500
+              secondary: '#e7f9ed', // light green
+            },
+          },
+          error: {
+            iconTheme: {
+              primary: '#ef4444',
+              secondary: '#ffeaea',
+            },
+          },
+        }}
+      />
+
+      {location.pathname !== '/checkout' && <CartMobileLink />}
     </GlobalProvider>
-  )
+  );
 }
 
-export default App
+export default App;

817a91685364e6a913bc9e9cced1b42670779fe8
sarrru <sharusura123@gmail.com>
2025-07-31 23:10:01 +0545

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': 'http://localhost:8080', // Proxy all /api requests to backend
+//     },
+//   },
+//   build: {
+//     sourcemap: false, //  Prevents source maps from being generated
+//   },
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './cert/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, './cert/cert.pem')),
+    },
+    port: 5173,
     proxy: {
-      '/api': 'http://localhost:8080', // Proxy all /api requests to backend
+      '/api': {
+        target: 'https://localhost:8443',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
   build: {
-    sourcemap: false, //  Prevents source maps from being generated
+    sourcemap: false,
   },
-})
+});
+
